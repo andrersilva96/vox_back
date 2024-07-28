c1160cc14b740ee44023e3b8e9dba828363c52b7
Andre Rodrigues <andre.rsilva96@hotmail.com>
2024-07-27 23:24:36 -0300

    5 - Simple CRUD from Company
    https://trello.com/c/9yjrV7dN

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,18 +2,74 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api/companies', name: 'company_')]
 class CompanyController extends AbstractController
 {
-    #[Route('/company', name: 'app_company')]
-    public function index(): JsonResponse
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CompanyController.php',
-        ]);
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('', name: 'list', methods: ['GET'])]
+    public function list(): JsonResponse
+    {
+        $this->json($this->entityManager->getRepository(Company::class)->findAll());
+    }
+
+    #[Route('', name: 'create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $company = new Company();
+        $company->setName($data['name']);
+
+        $this->entityManager->persist($company);
+        $this->entityManager->flush();
+
+        return $this->json($company);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(int $id): JsonResponse
+    {
+        $company = $this->entityManager->getRepository(Company::class)->find($id);
+        if (!$company) return $this->json(['message' => 'Company not found'], 404);
+
+        return $this->json($company);
+    }
+
+    #[Route('/{id}', name: 'update', methods: ['PUT'])]
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $company = $this->entityManager->getRepository(Company::class)->find($id);
+        if (!$company) return $this->json(['message' => 'Company not found'], 404);
+
+        $company->setName($data['name']);
+        $this->entityManager->flush();
+
+        return $this->json($company);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(int $id): JsonResponse
+    {
+        $company = $this->entityManager->getRepository(Company::class)->find($id);
+
+        if ($company) {
+            $this->entityManager->remove($company);
+            $this->entityManager->flush();
+        }
+
+        return $this->json(['message' => 'Company deleted']);
     }
 }
